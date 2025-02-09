6a49dba0ce58f6e0faef0646d56b00bccf75745a
𝐱𝐂𝐇𝐀𝐌𝐎 🧡⚡ <herokubot458@gmail.com>
2025-02-09 13:27:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,6 +13,6 @@ PREFIX: process.env.PREFIX || '.' ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,   
 MAX_SIZE: 500,
 STATUS_REPLY_MESSAGE: '𝐘𝐎𝐔𝐑 𝐒𝐓𝐀𝐓𝐔𝐒 𝐒𝐄𝐄𝐍 𝐉𝐔𝐒𝐓 𝐍𝐎𝐖 𝐁𝐘 𝐱𝐂𝐇𝐀𝐌𝐎 ✅',    
-ALIVE:  process.env.ALIVE  || '> 𝐱𝐂𝐇𝐀𝐌𝐎'  ,    
+ALIVE:  process.env.ALIVE  || '> x𝐂𝐇𝐀𝐌𝐎'  ,    
 DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '' : process.env.DELETEMSGSENDTO        
 };
